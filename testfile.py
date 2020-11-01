b665130f781547f10505bfeb033f0db5a8643493
Xiao Zhao <63480584+xizh01@users.noreply.github.com>
2020-11-01 21:56:27 +0100

    update py

--- testfile.py
+++ testfile.py
@@ -1,15 +1,1 @@
-print(1)
-
-origin
-
-worked on feature branch
-
-add something: feature branch (get merged)
-  
-changed from github
-
-change main
-
-changed main from github
-
-1078
+main changed
